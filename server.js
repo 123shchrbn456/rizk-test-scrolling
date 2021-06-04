994739f1a97c2e01706777a62d958540ddbed1ae
123shchrbn456 <62572982+123shchrbn456@users.noreply.github.com>
2021-06-04 12:38:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const config = require('config');
 const path = require("path");
 const connectDB = require("./config/db");
 
@@ -19,15 +20,14 @@ app.use("/api/betslip", require("./routes/betslip"));
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
     // Set static folder
-    app.use(express.static("client/build"));
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
-    // любой route кроме определенных выше, загружаем файл из реакта client/build/index.html
-    app.get("*", (req, res) =>
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-    );
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	})
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = config.get('port') || 5000;
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT} !`);
